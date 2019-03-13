78da07afbaffb011abba7e01f7e39c335a055cb5
Ubuntu <ubuntu@ip-172-31-21-130.us-east-2.compute.internal>
2019-03-13 22:20:01 +0000

    initcommit

---
+++ generate.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+import configparser
+import datetime
+from email.utils import formatdate
+import os
+import re
+
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+
+
+headerxml = '''<rss version= "2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd"><channel>
+                    <atom:link href="''' + config['global']['externalSiteRoot'] + '/' + config['global']['feedLocation'] + '''" rel="self" type="application/rss+xml" />
+                    <title>''' + config['global']['title'] + '''</title>
+                    <link>''' + config['global']['externalSiteRoot']+ '''</link>
+                    <description>''' + config['global']['description'] + '''</description>
+                    <language>''' + config['global']['language'] + '''</language>
+                    <lastBuildDate> '''+ formatdate() +'''</lastBuildDate>
+                    <generator>Nate's sweet custom podcast generator</generator>
+                    <webMaster>''' + config['global']['ownerEmail'] +' (' + config['global']['ownerName'] + ')' '''</webMaster>
+                        <image>
+                        <url>''' + config['global']['externalSiteRoot']+'/'+config['global']['imageUrlFromRoot'] + '''</url>
+                        <link>''' + config['global']['externalSiteRoot'] + '''</link>
+                        <title>''' + config['global']['title'] + '''</title> 
+                        </image>
+                    <itunes:author>''' + config['global']['ownerName'] + '''</itunes:author>
+                    <itunes:explicit>''' + config['global']['explicit'] + '''</itunes:explicit>
+                    <itunes:owner>
+                    <itunes:email>''' + config['global']['ownerEmail'] + '''</itunes:email>
+                    <itunes:name>''' + config['global']['ownerName'] + '''</itunes:name>
+                    </itunes:owner>
+                    <itunes:category text="''' + config['global']['category']+ '''"/>'''
+
+tailxml = '''</channel></rss>'''
+
+episodes = ''
+for section in config.sections():
+    
+    if section == 'global':
+        pass
+    else:
+        releasedate = datetime.datetime.strptime(config[section]['releaseDate'], '%Y %b %d')
+        if releasedate < datetime.datetime.now():
+            
+            episodes += '''
+                    <item>
+                    <enclosure url="''' + config['global']['externalSiteRoot'] + '/' + config[section]['link'] + '\" length=\"' + str(os.path.getsize(config['global']['serverSiteRoot'] + '/' + config[section]['link'])) + '''" type="audio/mpeg"/>
+                    <title>''' + section + '''</title>
+                    <description>''' + config[section]['description'] + '''</description>
+                    <pubDate>''' + formatdate(float(releasedate.strftime('%s'))) + '''</pubDate>
+                    <guid isPermaLink="false">''' + config['global']['externalSiteRoot'] + '/#' + config[section]['uniqueid'] + '''</guid>
+                    <link>''' + config['global']['externalSiteRoot'] + '/'+ config[section]['link'] + '''</link>
+                    <itunes:keywords>''' + config[section]['itunesKeywords'] + '''</itunes:keywords>
+                    <itunes:duration>''' + str(config[section]['duration']) +'''</itunes:duration>
+                    </item>
+                    '''    
+
+print(headerxml + episodes + tailxml)
