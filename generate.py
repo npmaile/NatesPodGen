4b50962e1dca1916c81449e78e8bbcc0acccf6a4
Ubuntu <ubuntu@ip-172-31-21-130.us-east-2.compute.internal>
2019-03-14 06:37:39 +0000

    changed the generate file to do the whole generation process + cleanup

--- generate.py
+++ generate.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import xml.dom.minidom
 import configparser
 import datetime
 from email.utils import formatdate
@@ -9,29 +10,27 @@ import re
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-
-
 headerxml = '''<rss version= "2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd"><channel>
-                    <atom:link href="''' + config['global']['externalSiteRoot'] + '/' + config['global']['feedLocation'] + '''" rel="self" type="application/rss+xml" />
-                    <title>''' + config['global']['title'] + '''</title>
-                    <link>''' + config['global']['externalSiteRoot']+ '''</link>
-                    <description>''' + config['global']['description'] + '''</description>
-                    <language>''' + config['global']['language'] + '''</language>
-                    <lastBuildDate> '''+ formatdate() +'''</lastBuildDate>
+                    <atom:link href="'''        + config['global']['externalSiteRoot'] + '/' + config['global']['feedLocation'] + '''" rel="self" type="application/rss+xml" />
+                    <title>'''                  + config['global']['title'] + '''</title>
+                    <link>'''                   + config['global']['externalSiteRoot']+ '''</link>
+                    <description>'''            + config['global']['description'] + '''</description>
+                    <language>'''               + config['global']['language'] + '''</language>
+                    <lastBuildDate> '''         + formatdate() +'''</lastBuildDate>
                     <generator>Nate's sweet custom podcast generator</generator>
-                    <webMaster>''' + config['global']['ownerEmail'] +' (' + config['global']['ownerName'] + ')' '''</webMaster>
-                        <image>
-                        <url>''' + config['global']['externalSiteRoot']+'/'+config['global']['imageUrlFromRoot'] + '''</url>
-                        <link>''' + config['global']['externalSiteRoot'] + '''</link>
-                        <title>''' + config['global']['title'] + '''</title> 
-                        </image>
-                    <itunes:author>''' + config['global']['ownerName'] + '''</itunes:author>
-                    <itunes:explicit>''' + config['global']['explicit'] + '''</itunes:explicit>
+                    <webMaster>'''              + config['global']['ownerEmail'] +' (' + config['global']['ownerName'] + ')' '''</webMaster>
+                    <image>
+                        <url>'''                + config['global']['externalSiteRoot']+'/'+config['global']['imageUrlFromRoot'] + '''</url>
+                        <link>'''               + config['global']['externalSiteRoot'] + '''</link>
+                        <title>'''              + config['global']['title'] + '''</title> 
+                    </image>
+                    <itunes:author>'''          + config['global']['ownerName'] + '''</itunes:author>
+                    <itunes:explicit>'''        + config['global']['explicit'] + '''</itunes:explicit>
                     <itunes:owner>
-                    <itunes:email>''' + config['global']['ownerEmail'] + '''</itunes:email>
-                    <itunes:name>''' + config['global']['ownerName'] + '''</itunes:name>
+                        <itunes:email>'''       + config['global']['ownerEmail'] + '''</itunes:email>
+                        <itunes:name>'''        + config['global']['ownerName'] + '''</itunes:name>
                     </itunes:owner>
-                    <itunes:category text="''' + config['global']['category']+ '''"/>'''
+                    <itunes:category text="'''  + config['global']['category']+ '''"/>'''
 
 tailxml = '''</channel></rss>'''
 
@@ -46,15 +45,16 @@ for section in config.sections():
             
             episodes += '''
                     <item>
-                    <enclosure url="''' + config['global']['externalSiteRoot'] + '/' + config[section]['link'] + '\" length=\"' + str(os.path.getsize(config['global']['serverSiteRoot'] + '/' + config[section]['link'])) + '''" type="audio/mpeg"/>
-                    <title>''' + section + '''</title>
-                    <description>''' + config[section]['description'] + '''</description>
-                    <pubDate>''' + formatdate(float(releasedate.strftime('%s'))) + '''</pubDate>
+                    <enclosure url="'''         + config['global']['externalSiteRoot'] + '/' + config[section]['link'] + '\" length=\"' + str(os.path.getsize(config['global']['serverSiteRoot'] + '/' + config[section]['link'])) + '''" type="audio/mpeg"/>
+                    <title>'''                  + section + '''</title>
+                    <description>'''            + config[section]['description'] + '''</description>
+                    <pubDate>'''                + formatdate(float(releasedate.strftime('%s'))) + '''</pubDate>
                     <guid isPermaLink="false">''' + config['global']['externalSiteRoot'] + '/#' + config[section]['uniqueid'] + '''</guid>
-                    <link>''' + config['global']['externalSiteRoot'] + '/'+ config[section]['link'] + '''</link>
-                    <itunes:keywords>''' + config[section]['itunesKeywords'] + '''</itunes:keywords>
-                    <itunes:duration>''' + str(config[section]['duration']) +'''</itunes:duration>
+                    <link>'''                   + config['global']['externalSiteRoot'] + '/'+ config[section]['link'] + '''</link>
+                    <itunes:keywords>'''        +config[section]['itunesKeywords'] + '''</itunes:keywords>
+                    <itunes:duration>'''        + str(config[section]['duration']) +'''</itunes:duration>
                     </item>
-                    '''    
-
-print(headerxml + episodes + tailxml)
+                    '''
+rssxml = xml.dom.minidom.parseString(headerxml + episoded + tailxml)
+with open("site/podcast.rss",'w+') as rssfeed:
+    rssfeed.write(rssxml.toprettyxml())
