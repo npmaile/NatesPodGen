e3d26e5e9b7a1df08fc314b7cd403b8eed379ef9
Ubuntu <ubuntu@ip-172-31-21-130.us-east-2.compute.internal>
2019-03-14 05:38:23 +0000

    fixed the template and the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "Welcome to Nate's podcast generator"
 WorkingDirectory=$(pwd)
-echo "first things first, what is the fully qualified domain name you wish to host this podcast on\n format: subdomain.website.tld"
+echo -e "first things first, what is the fully qualified domain name you wish to host this podcast on\n format: subdomain.website.tld"
 read FQDN
 
 while true; do
@@ -13,12 +13,13 @@ while true; do
 	esac
 done
 
+echo $workingDirectory
+cat templates/podcast.conf | sed "s/\$FQDN/$FQDN/g" | sed "s|\$workingdirectory|$WorkingDirectory|g" > config/podcast.conf
 
-cat templates/podcast.conf | sed "s/\$FQDN/$FQDN/g" | sed "s/\$workingdirectory/$WorkingDirectory/g" > config/podcast.conf
+cat templates/feed.ini  | sed "s/\$FQDN/$FQDN/g" | sed "s|\$workingdirectory|$WorkingDirectory|g" > feed.ini
+cat templates/general.conf > config/general.conf
 
-cat templates/feed.ini  | sed "s/\$FQDN/$FQDN/g" | sed "s/\$workingdirectory/$WorkingDirectory/g" > feed.ini
-
-echo "if there were no errors, the process is complete\
+echo -e "if there were no errors, the process is complete\
 	\nThe next steps are as follows\
 	\n1. add the nginx directive of inclucde $WorkingDirectory/config/podcast.conf\
 	\n2. Set up ssl certificates for this site (letsencrypt has a script that will do this automagically)\
