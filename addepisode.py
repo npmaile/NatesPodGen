c7fa38b2891c8b74129cc99cebe43c36f23e4e81
npmaile <npmaile@gmail.com>
2019-03-21 06:07:44 +0000

    It works. is this what 1.0 looks like?

--- addepisode.py
+++ addepisode.py
@@ -1,40 +1,47 @@
 #!/usr/bin/env python3
 import os
-import subprocess
+import shutil
 import argparse
 import configparser
 import re
 import datetime
-    
+import time    
 parser = argparse.ArgumentParser(description="add an entry to feed.ini")
 parser.add_argument('mp3file', type=str , nargs=1 , help='The mp3 file to be added to your feed')
 parser.add_argument('-i','--image',type=str,nargs=1,default = None ,help='the optional episode specific image for your episode')
 args = parser.parse_args()
-mp3filepath = args['mp3file']
+mp3filepath = vars(args)['mp3file'][0]
+
+print(mp3filepath)
 
 def calclength(mp3file):
-    answer = input('what is the length of your mp3 audio file?(HH:MM:SS)'
-            while not re.match('[1-9]2:[1-9]2:[1-9]2',answer):
-                answer = input('Please put it in the correct format')##todo generate this instead of asking for it because I'm a good programmer, Damnit!
+    answer = input('what is the length of your mp3 audio file?(HH:MM:SS)')
+    while not re.match('[0-9][0-9]:[0-9][0-9]:[0-9][0-9]',answer):
+        answer = input('Please put it in the correct format')##todo generate this instead of asking for it because I'm a good programmer, Damnit!
+    return answer
 
 def getuuid():
-    inifile = configparser.read_file('feed.ini')
+    config = configparser.ConfigParser()
+    config.read('feed.ini')
     maxnumber = 0
-    for x in inifile.sections():
-        if inifile[x]['uuid'] is not None:
-            if int(inifile[x]['uuid']) > maxnumber:
-                maxnumber = int(inifile[x]['uuid'])
+    for x in config.sections():
+        try:
+            if config[x]['uuid'] is not None:
+                if int(config[x]['uuid']) > maxnumber:
+                    maxnumber = int(config[x]['uuid'])
+        except KeyError:
+            pass
 
     return maxnumber
     
 
 def askforreleasedate():
     validMonths = ('jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec')
-    today = datetime.datetime.today()
-    month,date,year = today.month,today.day,today.year
     answer = input('Would you like to release immediately?[y,N]')
-    if answer.charAt(0).lower() is 'y':
-        return "%s %s %s".format(str(year),str(month),str(day))
+    if answer[0].lower() == 'y':
+        today = datetime.date.fromtimestamp(time.time())
+        return today.strftime('%Y %b %d')
+
     else:
         print('tough tiddies')
         #todo: ask for the release date
@@ -43,35 +50,36 @@ image = None
    
 uniqueid = getuuid() + 1
 title = input("Title: ")
-title = uniqueid + ':' + description
 description = input("Description")
+title = str(uniqueid) + ':' + description
 itunesCategory = input("itunes Category")
 itunesKeywords = input("itunes Keywords(separated by commas)") 
 releaseDate = askforreleasedate()
-duration = calculateduration(mp3filepath)
+duration = calclength(mp3filepath)
 
-link = 'site/episodes/' + title
-print('copying the mp3 file to' + link)
-subprocess.call(['cp',mp3filepath,link])
+link = 'episodes/' + title
+print('copying the mp3 file to' + 'site/' + link)
+shutil.copy(mp3filepath,'site/' + link)
 
-if args['image'] is not None:
-    image = args['image']
+if vars(args)['image'] is not None:
+    image = args['image'][0]
     imagePath = 'site/images/' + uniqueid + ':' + description
     print('copying the image file to' + imagePath)
-    subprocess.call(['cp',image,imagepath])
+    shutil.copy(image,imagepath)
  
 
 
 with open("feed.ini",'a') as feedfile:
-    feedfile.write('[' + title + ']')
-    feedfile.write('description = ' + description)
-    feedfile.write('releaseDate = ' + releaseDate)
-    feedfile.write('uniqueid = ' + uniqueid)
-    feedfile.write('itunesCategory = ' + itunesCategory)
-    feedfile.write('duration = ' + duration)
-    feedfile.write('itunesKeywords = ' + itunesKeywords)
+    feedfile.write('[' + title + ']\n')
+    feedfile.write('description = ' + description + '\n')
+    feedfile.write('releaseDate = ' + releaseDate + '\n')
+    feedfile.write('uniqueid = ' + str(uniqueid) + '\n')
+    feedfile.write('itunesCategory = ' + itunesCategory + '\n')
+    feedfile.write('duration = ' + duration + '\n')
+    feedfile.write('itunesKeywords = ' + itunesKeywords + '\n')
+    feedfile.write('link = ' + link + '\n')
     if image is not None:
-        feedfile.write('image = ' + imagepath)
+        feedfile.write('image = ' + imagepath + '\n')
 
 
 
