81d1e5591cd913480763ebd6aee1fe3ec4fe339d
npmaile <npmaile@gmail.com>
2019-03-25 17:10:52 -0500

    in the middle of a refactor

--- generate.py
+++ generate.py
@@ -6,22 +6,76 @@ from email.utils import formatdate
 import os
 import re
 
+class Episode:
+    def __init__(self, link, length, title, description, releasedate, uniqueId, keywords, duration, altImage = None)
+        self.link = link
+        self.length = length
+        self.title = title
+        self.description = description 
+        self.releasedate = releasedate
+        self.uniqueId = uniqueId
+        self.keywords = keywords
+        self.duration = duration
+        self.altImage = altImage
 
-config = configparser.ConfigParser()
-config.read('feed.ini')
-def genRss():
+class Podcast:
+    def __init__(self,externalroot,feedlocation,title,description,language,imagelocation,explicit,ownername,owneremail,category))
+        self.episodes = []
+        self.externalroot = externalroot
+        self.feedlocation = feedlocation
+        self.title = title
+        self.description = description
+        self.language = language
+        self.builddate = builddate
+        self.imagelocation = imagelocation
+        self.explicit = explicit
+        self.ownername = ownername
+        self.owneremail = owneremail
+        self.category = category
+        self.builddate = formatdate()
+def PopulateClasses(config):
+    globalconfig = config['global']
+    Cast = Podcast(\
+            externalroot = globalconfig['externalSiteRoot'],\
+            feedlocation = globalconfig['feedlocation'],\
+            title = globalconfig['title'],\
+            description = globalconfig['description'],\
+            language = globalconfig['language'],\    
+            imagelocation = globalconfig['imageUrlFromRoot'],\
+            explicit = globalconfig['explicit'],\ 
+            ownername = globalconfig['ownerName'],\
+            owneremail = globalconfig['ownerEmail'],\
+            category = globalconfig['category'])
+
+    for episode in config.sections():
+        if episode == 'global':
+            pass
+        else:
+            ep = config[episode]
+            Cast.append(Episode(\
+                    link = ep['link'],\
+                    length = str(os.path.getsize(config['global']['serverSiteRoot'] + '/' + config[section]['link'])),\
+                    title = ep['title'],\
+                    description = ep['description'],\
+                    releasedate = formatdate(float(datetime.datetime.strptime(ep['releaseDate'],  '%Y %b %d').strftime('%s']))),\
+                    uniqueId = ep['uniqueid'],\
+                    keywords = ep['itunesKeywords'],\
+                    duration = ep['duration']))
+
+def genRss(config):
+    podcast = PopulateClasses(config)
 
     headerxml = '''<rss version= "2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd"><channel>
-                        <atom:link href="'''        + config['global']['externalSiteRoot'] + '/' + config['global']['feedLocation'] + '''" rel="self" type="application/rss+xml" />
-                        <title>'''                  + config['global']['title'] + '''</title>
-                        <link>'''                   + config['global']['externalSiteRoot']+ '''</link>
-                        <description>'''            + config['global']['description'] + '''</description>
-                        <language>'''               + config['global']['language'] + '''</language>
+                        <atom:link href="'''        + podcast.externalroot + '/' + podcast.feedlocation + '''" rel="self" type="application/rss+xml" />
+                        <title>'''                  + podcast.title + '''</title>
+                        <link>'''                   + podcast.link + '''</link>
+                        <description>'''            + podcast.description + '''</description>
+                        <language>'''               + podcast.language + '''</language>
                         <lastBuildDate> '''         + formatdate() +'''</lastBuildDate>
                         <generator>Nate's sweet custom podcast generator</generator>
-                        <webMaster>'''              + config['global']['ownerEmail'] +' (' + config['global']['ownerName'] + ')' '''</webMaster>
+                        <webMaster>'''              + podcast.owneremail +' (' + podcast.ownername + ')' '''</webMaster>
                         <image>
-                            <url>'''                + config['global']['externalSiteRoot']+'/'+config['global']['imageUrlFromRoot'] + '''</url>
+                            <url>'''                + podcast.externalroot +'/'+ podcast.imagelink + '''</url>
                             <link>'''               + config['global']['externalSiteRoot'] + '''</link>
                             <title>'''              + config['global']['title'] + '''</title> 
                         </image>
@@ -61,5 +115,8 @@ def genRss():
     with open("site/podcast.rss",'w+') as rssfeed:
         rssfeed.write(rssxml.toprettyxml())
 
+configfile = configparser.ConfigParser()
+config.read('feed.ini')
+with open("site/podcast.rss",'w+') as rssfeed:
+    rssfeed.write(genRss(config))
 
-genRss()
